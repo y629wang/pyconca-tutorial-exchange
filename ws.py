9bf8ba2ef4b72c86480c185c540ae8ff1920773b
Ankit Mittal <ankitml@gmail.com>
2018-09-11 14:47:11 -0400

    web sockets to redis communication flowing well

--- ws.py
+++ ws.py
@@ -1,55 +1,38 @@
 import asyncio
-from aioredis import create_connection, Channel
-import json
+import aioredis
+from aioredis.pubsub import Receiver
 import websockets
-import time
 
 
-async def subscribe_to_redis():
-    conn = await create_connection(('localhost', 6379))
-    channel = Channel('*', is_pattern=True)
-    await conn.execute_pubsub('subscribe', channel)
-    return channel, conn
+class RedisToWS:
+
+    def __init__(self):
+        self.loop = asyncio.get_event_loop()
+        self.loop.set_debug(True)
+        self.channel = 'updates'
+
+    def start(self):
+        #self.loop.run_until_complete(self.receive())
+        self.loop.run_until_complete(
+            websockets.serve(self.ws_handler, 'localhost', 8765),
+        )
+        self.loop.run_forever()
+
+    async def ws_handler(self, websocket, path):
+        async for message in self.receive():
+            print(message)
+            await websocket.send(message)
+
+    async def receive(self):
+        print('receive starting from redis')
+        connection = await aioredis.create_connection(('localhost', 6379))
+        receiver = Receiver()
+        connection.execute_pubsub('subscribe', receiver.channel(self.channel))
+        while (await receiver.wait_message()):
+            message = await receiver.get()
+            yield message[1].decode('utf-8')
 
 
-async def redis_sub_handler(websocket):
-    channel, conn = await subscribe_to_redis()
-    while True:
-        message = await channel.get()
-        print(message)
-        await websocket.send(message)
-
-
-async def heartbeat():
-    return {'OK':'OK'}
-
-
-async def heartbeat_handler(websocket):
-    while True:
-        result = await heartbeat()
-        await websocket.send(json.dumps(result))
-        await asyncio.sleep(5)
-
-
-async def websocket_handler(websocket, path):
-    #heartbeat_task= asyncio.ensure_future(
-    #    heartbeat_handler(websocket)
-    #)
-    redis_task = asyncio.ensure_future(
-        redis_sub_handler(websocket)
-    )
-
-    done, pending = await asyncio.wait(
-        [redis_task],
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-    for task in pending:
-        task.cancel()
-
-if __name__=='__main__':
-    loop = asyncio.get_event_loop()
-    loop.set_debug(True)
-    loop.run_until_complete(
-        websockets.serve(redis_sub_handler, 'localhost', 8765),
-    )
-    loop.run_forever()
+if __name__ == '__main__':
+    feed = RedisToWS()
+    feed.start()
